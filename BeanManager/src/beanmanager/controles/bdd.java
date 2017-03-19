8b4bf85432ca238fc934437e062a28359d77da3c
Carlos <ascencio1994_05@hotmail.com>
2017-03-19 17:09:39 -0600

    Primera subida, bases para empezar la programacion de Bean Manager. Carpetas designadas: Solicitudes (Villa) Proyectos 	(Ascencio) Usuarios 	(Escrich) Ajustes		(Angel)

--- BeanManager/src/beanmanager/controles/bdd.java
+++ BeanManager/src/beanmanager/controles/bdd.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package beanmanager.controles;
-
+import java.sql.*;
+import java.sql.DriverManager;
 /**
  *
  * @author ascencio
  */
 public class bdd {
-    
+     Connection con;
+    public Connection bdd()
+    {
+       
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con =DriverManager.getConnection("jdbc:mysql://sql112.eshost.com.ar/eshos_19849355_beanmanager","eshos_19849355","Bean2017");  
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR_ "+e);     
+        }
+        return con;
+    }
+    public void close()
+    {
+        try
+        {
+             con.close();
+        }
+        catch(Exception e)
+        {
+             System.out.println("ERROR_ "+e);
+        }
+    }
 }
