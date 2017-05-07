a9bdc3f05c08140bcc3bca365b7389e8ecc9d47a
monicaescrich <monica.escrichc@gmail.com>
2017-05-07 17:00:16 -0600

    ConfPerfil

--- src/java/controladores/usuarios/usuarios.java
+++ src/java/controladores/usuarios/usuarios.java
@@ -10,5 +10,199 @@ package controladores.usuarios;
  * @author monica
  */
 public class usuarios {
-    
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre; 
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the fechanac
+     */
+    public String getFechanac() {
+        return fechanac;
+    }
+
+    /**
+     * @param fechanac the fechanac to set
+     */
+    public void setFechanac(String fechanac) {
+        this.fechanac = fechanac;
+    }
+
+    /**
+     * @return the presentacion
+     */
+    public String getPresentacion() {
+        return presentacion;
+    }
+
+    /**
+     * @param presentacion the presentacion to set
+     */
+    public void setPresentacion(String presentacion) {
+        this.presentacion = presentacion;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    /**
+     * @return the ciudad
+     */
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    /**
+     * @param ciudad the ciudad to set
+     */
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the paginaweb
+     */
+    public String getPaginaweb() {
+        return paginaweb;
+    }
+
+    /**
+     * @param paginaweb the paginaweb to set
+     */
+    public void setPaginaweb(String paginaweb) {
+        this.paginaweb = paginaweb;
+    }
+
+    /**
+     * @return the facebook
+     */
+    public String getFacebook() {
+        return facebook;
+    }
+
+    /**
+     * @param facebook the facebook to set
+     */
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    /**
+     * @return the twitter
+     */
+    public String getTwitter() {
+        return twitter;
+    }
+
+    /**
+     * @param twitter the twitter to set
+     */
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    /**
+     * @return the github
+     */
+    public String getGithub() {
+        return github;
+    }
+
+    /**
+     * @param github the github to set
+     */
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    /**
+     * @return the contrasena1
+     */
+    public String getContrasena1() {
+        return contrasena1;
+    }
+
+    /**
+     * @param contrasena1 the contrasena1 to set
+     */
+    public void setContrasena1(String contrasena1) {
+        this.contrasena1 = contrasena1;
+    }
+
+    /**
+     * @return the contrasena2
+     */
+    public String getContrasena2() {
+        return contrasena2;
+    }
+
+    /**
+     * @param contrasena2 the contrasena2 to set
+     */
+    public void setContrasena2(String contrasena2) {
+        this.contrasena2 = contrasena2;
+    }
+    private String nombre; 
+    private String apellido; 
+    private String fechanac; 
+    private String presentacion; 
+    private String pais; 
+    private String ciudad; 
+    private String correo;
+    private String paginaweb; 
+    private String facebook; 
+    private String twitter; 
+    private String github; 
+    private String contrasena1; 
+    private String contrasena2; 
 }
